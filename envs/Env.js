f3f78feb701531e433b16b1e484d13a9e3f33855
Lihano <64770899+ihan12389@users.noreply.github.com>
2021-08-02 13:03:42 +0900

    Delete envs directory

--- envs/Env.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const REACT_APP_API_KEY = "AIzaSyB7-jd-_VfwiBXESzO-PUU_FOR7aRtD48w";
-export const REACT_APP_AUTH_DOMAIN = "nwitter-79fb0.firebaseapp.com";
-export const REACT_APP_DATABASE_URL = "https://nwitter-79fb0.firebase.io.com";
-export const REACT_APP_PROJECT_ID = "nwitter-79fb0";
-export const REACT_APP_STORAGE_BUCKET = "nwitter-79fb0.appspot.com";
-export const REACT_APP_MESSAGIN_ID = "755424085291";
-export const REACT_APP_APP_ID = "1:755424085291:web:d3b15de812656adf63316e";
